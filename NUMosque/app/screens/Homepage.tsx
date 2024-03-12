8915b0e7093ab3794a3cc137b20212db9fa199d9
moetri17 <mohamedetri667@gmail.com>
2024-03-12 01:58:10 +0000

    countdown.tsx

--- NUMosque/app/screens/Homepage.tsx
+++ NUMosque/app/screens/Homepage.tsx
@@ -1,22 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
-import { BlurView } from 'expo-blur';
+import { View, Text, Image, StyleSheet, Dimensions, ScrollView } from 'react-native';
 import Countdown from '../../components/CountDown';
-
+import PrayerTimes from '../../components/PrayerTime';
 
 const screenHeight = Dimensions.get('window').height;
 
 const Homepage = () => {
-  const [nextPrayer, setNextPrayer] = useState({ name: '', time: '' });
+  const [prayerTimes, setPrayerTimes] = useState([]);
+  const [currentDate, setCurrentDate] = useState(new Date());
 
   useEffect(() => {
     const fetchPrayerTimes = async () => {
       try {
-        // Ensure this URL is correct and accessible from your device
         const response = await fetch('http://192.168.0.23:8000/api/prayer-times');
         const data = await response.json();
-
-        setNextPrayer({ name: data.nextPrayerName, time: data.nextPrayerTime });
+        setPrayerTimes(data);
       } catch (error) {
         console.error("Failed to fetch prayer times:", error);
       }
@@ -25,52 +23,37 @@ const Homepage = () => {
     fetchPrayerTimes();
   }, []);
 
-  const targetDate = new Date().toISOString().slice(0, 10) + 'T' + nextPrayer.time;
+  const isNextPrayer = (prayer) => {
+    const now = new Date();
+    const today = now.toISOString().split('T')[0];
+    const prayerTimeDate = new Date(`${today}T${prayer.Time}`);
+    return now < prayerTimeDate;
+  };
+
+  const formattedDate = new Intl.DateTimeFormat('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }).format(currentDate);
+  const nextPrayerIndex = prayerTimes.findIndex(isNextPrayer);
 
   return (
     <>
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
         <View style={logoStyles.logoContainer}>
           <Image source={require('./assets/mosqueInside.jpg')} style={logoStyles.image}/> 
-          <BlurView intensity={0} style={StyleSheet.absoluteFill}>   
-            <View>  
-              <Image source={require('./assets/Mosque_logo.png')} style={logoStyles.logo}/>
-              <View style={logoStyles.textContainer}>  
-                <Text style={logoStyles.text}>
-                  Northumbria ISOC
-                </Text>
-                <Text style={logoStyles.text}>Newcastle upon Tyne NE1 8SU</Text>
-              </View>
+          <View>  
+            <Image source={require('./assets/Mosque_logo.png')} style={logoStyles.logo}/>
+            <View style={logoStyles.textContainer}>  
+              <Text style={logoStyles.text}>Northumbria ISOC</Text>
+              <Text style={logoStyles.text}>Newcastle upon Tyne NE1 8SU</Text>
             </View>
-          </BlurView>
+          </View>
         </View>
-
-        <View style={prayerStyles.prayerTimesContainer}>
-          <Text>The next prayer {nextPrayer.name} will be at: {nextPrayer.time}</Text>
-          {nextPrayer.time && <Countdown targetDate={targetDate} />}
+        <View style={date.dateContainer}>
+          <Text style={logoStyles.date}>{formattedDate}</Text>
         </View>
-
-
-        <View style={prayerStyles.prayerTimesContainer}>
-          <Text>Ayah for today:</Text>
-        </View>
-
-
-        <View style={prayerStyles.prayerTimesContainer}>
-          <Text>Upcoming events:</Text>
-        </View>
-
-        <View style={prayerStyles.prayerTimesContainer}>
-          <Text>hadith of the day:</Text>
-        </View>
-
-        <View style={prayerStyles.prayerTimesContainer}>
-          <Text>Ask me a question system:</Text>
-        </View>
-      </View>
+        <PrayerTimes prayerTimes={prayerTimes} nextPrayerIndex={nextPrayerIndex} />
+      </ScrollView>
     </>
   );
-}
+};
 
 export default Homepage;
 
@@ -89,6 +72,7 @@ const logoStyles = StyleSheet.create({
     justifyContent: 'center', // Center the content vertically
     backgroundColor: '#FFF9EF',
     alignItems: 'center',
+    width: '100%',
   },
   logo: {
     height: 75,
@@ -98,9 +82,9 @@ const logoStyles = StyleSheet.create({
     marginBottom: '2%',
   },
   image: {
-    position: 'absolute', // Make image fill the designated space without affecting layout
-    width: '100%', // Ensure the image covers the width
-    height: '100%', // The image height now effectively 25% of the screen height
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
   },
   textContainer: {
     alignItems: 'center',
@@ -110,17 +94,23 @@ const logoStyles = StyleSheet.create({
     fontSize: 15,
     color: 'white', // Ensure text is visible on the image
   },
+  date: { 
+    textAlign: 'left', 
+    marginVertical: 10, 
+    fontSize: 14, 
+    marginLeft:10
+  }
 });
 
-const prayerStyles = StyleSheet.create({
-  prayerTimesContainer: {
+const date = StyleSheet.create({
+  dateContainer: {
     flexDirection: 'column',
     backgroundColor: '#FFF9EF',
     margin: 10,
-    padding: 10,
+    padding: 5,
     borderStyle: 'solid',
     borderWidth: 1,
     borderRadius: 8,
-    alignSelf:'center'
+    alignSelf: 'center',
   },
 });
