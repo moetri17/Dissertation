2b9ff25436b754a36c5386e0ada0a087634730d3
moetri17 <mohamedetri667@gmail.com>
2024-05-08 03:30:10 +0100

    new admin

--- NUMosque/app/screens/AddAdmin.tsx
+++ NUMosque/app/screens/AddAdmin.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button, FlatList, Alert, ActivityIndicator } from 'react-native';
+import { View, Text, Button, FlatList, Alert, ActivityIndicator, StyleSheet } from 'react-native';
 
 const AddAdmin = () => {
   const [users, setUsers] = useState([]);
@@ -58,21 +58,20 @@ const AddAdmin = () => {
         console.error('Error:', error);
         Alert.alert('Error', error.message || 'Failed to update admin status');
     }
-};
-
+  };
 
   if (loading) return <ActivityIndicator size="large" color="#0000ff" />;
   if (error) return <Text>Error: {error}</Text>;
 
   return (
-    <View style={{ flex: 1, padding: 20 }}>
+    <View style={styles.container}>
       <FlatList
         data={users}
         keyExtractor={item => item.uid}
         renderItem={({ item }) => (
-          <View style={{ marginBottom: 20 }}>
-            <Text>{item.first_name || 'First Name not provided'} {item.last_name || 'Last Name not provided'}</Text>
-            <Text>{item.phone_number || 'Phone number not available'}</Text>
+          <View style={styles.userContainer}>
+            <Text style={styles.text}>{item.first_name || 'First Name not provided'} {item.last_name || 'Last Name not provided'}</Text>
+            <Text style={styles.text}>{item.phone_number || 'Phone number not available'}</Text>
             <Button
               title={item.is_admin ? 'Remove Admin' : 'Make Admin'}
               onPress={() => handleAdminToggle(item, !item.is_admin)}
@@ -83,5 +82,28 @@ const AddAdmin = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+  },
+  userContainer: {
+    backgroundColor: '#FFF8E1',
+    padding: 10,
+    borderRadius: 10,
+    margin: 10,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  text: {
+    fontWeight: '700',
+    fontSize: 15,
+    color: '#000', // Changed to black for readability
+  },
+});
 
 export default AddAdmin;
